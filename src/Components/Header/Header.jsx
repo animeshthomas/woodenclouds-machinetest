0103553d309ff9218c5df921a32eea1c49a6d9cd
Animesh Thomas <animeshthomas262@gmail.com>
2024-09-30 19:35:08 +0530

    bug change in sidebar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,55 +1,92 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import AvatarImage from '../../Data/AvatarImage.png';
 
 const Header = () => {
   return (
     <div>
-          <header className="app-header">
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <ul className="navbar-nav">
-        <li className="nav-item d-block d-xl-none">
-          <a className="nav-link sidebartoggler nav-icon-hover" id="headerCollapse" href="javascript:void(0)">
-            <i className="ti ti-menu-2" />
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link nav-icon-hover" href="javascript:void(0)">
-            <i className="ti ti-bell-ringing" />
-            <div className="notification bg-primary rounded-circle" />
-          </a>
-        </li>
-      </ul>
-      <div className="navbar-collapse justify-content-end px-0" id="navbarNav">
-        <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
-          <Link to="/addProducts" className="btn btn-primary">Add Products</Link>
-          <li className="nav-item dropdown">
-            <a className="nav-link nav-icon-hover" href="javascript:void(0)" id="drop2" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="../assets/images/profile/user-1.jpg" alt width={35} height={35} className="rounded-circle" />
-            </a>
-            <div className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-              <div className="message-body">
-                <a href="javascript:void(0)" className="d-flex align-items-center gap-2 dropdown-item">
-                  <i className="ti ti-user fs-6" />
-                  <p className="mb-0 fs-3">My Profile</p>
+      <header className="app-header">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <ul className="navbar-nav">
+            <li className="nav-item d-block d-xl-none">
+              <a
+                className="nav-link sidebartoggler nav-icon-hover"
+                id="headerCollapse"
+                href="javascript:void(0)"
+              >
+                <i className="ti ti-menu-2" />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link nav-icon-hover" href="javascript:void(0)">
+                <i className="ti ti-bell-ringing" />
+                <div className="notification bg-primary rounded-circle" />
+              </a>
+            </li>
+          </ul>
+          <div className="navbar-collapse justify-content-end px-0" id="navbarNav">
+            <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+              <Link to="/addProducts" className="btn btn-primary">Add Products</Link>
+              &nbsp; &nbsp; &nbsp;
+              <li className="nav-item dropdown d-flex align-items-center">
+                <img
+                  src={AvatarImage}
+                  alt="Avatar"
+                  width={35}
+                  height={35}
+                  className="rounded-circle"
+                />
+                <div className="d-flex flex-column mx-2">
+                  <span className="fw-bold">Animesh Thomas</span> 
+                  <span className="text-muted small">Owner</span> 
+                </div>
+                <a
+                  className="nav-link nav-icon-hover"
+                  href="javascript:void(0)"
+                  id="drop2"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="ti ti-chevron-down" />
                 </a>
-                <a href="javascript:void(0)" className="d-flex align-items-center gap-2 dropdown-item">
-                  <i className="ti ti-mail fs-6" />
-                  <p className="mb-0 fs-3">My Account</p>
-                </a>
-                <a href="javascript:void(0)" className="d-flex align-items-center gap-2 dropdown-item">
-                  <i className="ti ti-list-check fs-6" />
-                  <p className="mb-0 fs-3">My Task</p>
-                </a>
-                <a href="" className="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a>
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+                <div
+                  className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+                  aria-labelledby="drop2"
+                >
+                  <div className="message-body">
+                    <a
+                      href="javascript:void(0)"
+                      className="d-flex align-items-center gap-2 dropdown-item"
+                    >
+                      <i className="ti ti-user fs-6" />
+                      <p className="mb-0 fs-3">My Profile</p>
+                    </a>
+                    <a
+                      href="javascript:void(0)"
+                      className="d-flex align-items-center gap-2 dropdown-item"
+                    >
+                      <i className="ti ti-mail fs-6" />
+                      <p className="mb-0 fs-3">My Account</p>
+                    </a>
+                    <a
+                      href="javascript:void(0)"
+                      className="d-flex align-items-center gap-2 dropdown-item"
+                    >
+                      <i className="ti ti-list-check fs-6" />
+                      <p className="mb-0 fs-3">My Task</p>
+                    </a>
+                    <a href="" className="btn btn-outline-primary mx-3 mt-2 d-block">
+                      Logout
+                    </a>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
